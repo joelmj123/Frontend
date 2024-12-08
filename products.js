7a7f3927beec566334367450787832a226fde51d
joelmj123 <joelmj123@gmail.com>
2024-12-09 00:43:43 +0400

    initial commit

---
+++ products.js
@@ -0,0 +1,110 @@
+var clubs = [
+    {
+        id: 1,
+        title: "Art Club",
+        image: "images/art.jpeg",
+        description: "Explore your artistic skills!",
+        price: 20,
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id: 2,
+        title: "Coding Club",
+        image: "images/coding.jpg",
+        description: "Learn to code with fun projects!",
+        price: 25,
+        availableInventory: 15,
+        rating: 5
+    },
+    {
+        id: 3,
+        title: "Music Club",
+        image: "images/music.png",
+        description: "Master musical instruments and vocals!",
+        price: 30,
+        availableInventory: 12,
+        rating: 3
+    },
+    {
+        id: 4,
+        title: "Dance Club",
+        image: "images/dance.jpeg",
+        description: "Learn contemporary and classical dance!",
+        price: 18,
+        availableInventory: 20,
+        rating: 4
+    },
+    {
+        id: 5,
+        title: "Photography Club",
+        image: "images/photo.png",
+        description: "Capture stunning photographs and moments!",
+        price: 22,
+        availableInventory: 8,
+        rating: 5
+    },
+    {
+        id: 6,
+        title: "Science Club",
+        image: "images/Scienceclub.jpg",
+        description: "Dive into experiments and discoveries!",
+        price: 15,
+        availableInventory: 14,
+        rating: 4
+    },
+    {
+        id: 7,
+        title: "Drama Club",
+        image: "images/drama.png",
+        description: "Express yourself with theater arts!",
+        price: 28,
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id: 8,
+        title: "Sports Club",
+        image: "images/sports.jpg",
+        description: "Enjoy and excel in various sports!",
+        price: 20,
+        availableInventory: 18,
+        rating: 4
+    },
+    {
+        id: 9,
+        title: "Robotics Club",
+        image: "images/robotics.jpeg",
+        description: "Build and program robots!",
+        price: 40,
+        availableInventory: 7,
+        rating: 5
+    },
+    {
+        id: 10,
+        title: "Chess Club",
+        image: "images/chess.png",
+        description: "Sharpen your mind with strategic games!",
+        price: 12,
+        availableInventory: 25,
+        rating: 4
+    },
+    {
+        id: 11,
+        title: "Cooking Club",
+        image: "images/cooking.png",
+        description: "Master culinary skills with delicious recipes!",
+        price: 35,
+        availableInventory: 5,
+        rating: 5
+    },
+    {
+        id: 12,
+        title: "Nature Club",
+        image: "images/nature.jpeg",
+        description: "Explore and protect the environment!",
+        price: 16,
+        availableInventory: 10,
+        rating: 4
+    }
+];
